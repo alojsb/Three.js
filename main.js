4e0b21f155868dfdd8af5fdb5f55e6c5d2b177d1
alojsb <alojsbirmingham@gmail.com>
2024-08-11 23:05:11 +0200

    cylinder with gradient coloring

--- main.js
+++ main.js
@@ -1,33 +1,73 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-
 // initialize
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
-const rotationSpeed = 0.0005;
+camera.lookAt(scene.position);
+var renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+document.body.appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-// orbit controls
 new OrbitControls(camera, renderer.domElement);
 
-// tetrahedron
-const geo = new THREE.TetrahedronGeometry(2);
-const mat = new THREE.MeshNormalMaterial();
-const obj = new THREE.Mesh(geo, mat);
-scene.add(obj);
+var geometry = new THREE.CylinderGeometry(1, 1, 3, 24, 1, false);
+var material = new THREE.ShaderMaterial({
+  uniforms: {
+    color1: {
+      value: new THREE.Color("red")
+    },
+    color2: {
+      value: new THREE.Color("purple")
+    }
+  },
+  vertexShader: `
+    varying vec2 vUv;
+
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+    }
+  `,
+  fragmentShader: `
+    uniform vec3 color1;
+    uniform vec3 color2;
+  
+    varying vec2 vUv;
+    
+    void main() {
+      
+      gl_FragColor = vec4(mix(color1, color2, vUv.y), 1.0);
+    }
+  `,
+  wireframe: false
+});
+var mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 
-// White directional light at half intensity shining from the top.
-const directionalLight = new THREE.DirectionalLight( 0xffffff);
-directionalLight.position.set(-2, 0, 0);
-scene.add( directionalLight );
 
-// render loop
-function animate() {
-    renderer.render(scene, camera);
+render();
+
+function resize(renderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
 }
-renderer.setAnimationLoop(animate);
+
+function render() {
+  if (resize(renderer)) {
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+  }
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
